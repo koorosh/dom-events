1adfacb3efae2f9329498d9483564d3ae91f9617
Vorobiov, Andrii <Andrii.Vorobiov@b-next.com>
2016-02-19 18:27:17 +0200

    add dblclick and wheel events

--- dom-events.js
+++ dom-events.js
@@ -6,7 +6,8 @@
         proceed.apply(this, Array.prototype.slice.call(arguments, 1));
 
         var pointer = this,
-            container = pointer.chart.container;
+            container = pointer.chart.container,
+            DELAY = 500, clicks = 0, timer = null;
 
         container.oncontextmenu = function (e) {
             pointer.onContainerContextMenu(e);
@@ -19,6 +20,25 @@
             pointer.onWheel(e);
             e.stopPropagation();
             e.preventDefault();
+        };
+
+        // Override default click event handler by adding delay to handle double-click event
+        container.onclick = function (e) {
+            clicks++;
+            if(clicks === 1) {
+                timer = setTimeout(function() {
+                    pointer.onContainerClick(e);
+                    clicks = 0;
+                }, DELAY);
+            } else {
+                clearTimeout(timer);
+                clicks = 0;
+            }
+        };
+
+        container.ondblclick = function (e) {
+            pointer.onDblClick(e);
+            clicks = 0;
         };
     });
 
@@ -70,4 +90,45 @@
             }
         };
     }
+
+    var applyClickEventHandler = function( eventtype, propertyName ) {
+        if(!H.Pointer.prototype.hasOwnProperty(propertyName)) {
+            H.Pointer.prototype[propertyName] = function (e) {
+                var pointer = this,
+                    chart = pointer.chart,
+                    hoverPoint = chart.hoverPoint,
+                    plotLeft = chart.plotLeft,
+                    plotTop = chart.plotTop;
+
+                e = this.normalize(e);
+
+                if (!chart.cancelClick) {
+                    // On tracker click, fire the series and point events. #783, #1583
+                    if (hoverPoint && this.inClass(e.target, 'tracker')) {
+
+                        // the series click event
+                        fireEvent(hoverPoint.series, eventtype, $.extend(e, {
+                            point: hoverPoint
+                        }));
+
+                        // the point click event
+                        if (chart.hoverPoint) {
+                            hoverPoint.firePointEvent(eventtype, e);
+                        }
+                    } else {
+                        $.extend(e, this.getCoordinates(e));
+                        if (chart.isInsidePlot(e.chartX - plotLeft, e.chartY - plotTop)) {
+                            fireEvent(chart, eventtype, e);
+                        }
+                    }
+                }
+            };
+        }
+    };
+
+    applyClickEventHandler('contextmenu', 'onContainerContextMenu');
+
+    applyClickEventHandler('dblclick', 'onDblClick');
+
+
 }(Highcharts, jQuery));
